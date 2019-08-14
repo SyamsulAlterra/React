60959fd2de4ce853c8d14dc726e023227a6e8335
Syamsul <syamsul@alterra.id>
2019-08-14 18:32:11 +0700

    deploy at syamsulreact.surge.sh

---
+++ Challenge 3 & 4/build/precache-manifest.e5d807305ddfcefd5fca9d8b4108fc00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092b5bb02ce1b8b57f7b5f29ffb5f28d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b301f92db4cd1c8c6c0",
+    "url": "/static/css/main.718a8a41.chunk.css"
+  },
+  {
+    "revision": "7e3d8f08b16df783204d",
+    "url": "/static/js/2.77c40224.chunk.js"
+  },
+  {
+    "revision": "2b301f92db4cd1c8c6c0",
+    "url": "/static/js/main.8e97083c.chunk.js"
+  },
+  {
+    "revision": "322fd1165179b782628a",
+    "url": "/static/js/runtime~main.cd3fad76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
